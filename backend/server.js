d79610bf4001de13c9ea25cd30308ed26048cb19
Themanh99 <themanhchu99@gmail.com>
2021-05-14 11:14:43 +0700

    aa
    aa

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.get('*', (req, res) =>
 app.use((err , req , res , next ) =>{
     res.status(500).send({ message: err.message});
 });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
